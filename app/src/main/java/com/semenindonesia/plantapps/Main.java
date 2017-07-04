dbcf454972caf0d6e5a61021c1ba1d2d9a8d11a9
agungxfz <agungxfz@gmail.com>
2017-07-04 09:52:14 +0700

    first

---
+++ app/src/main/java/com/semenindonesia/plantapps/Main.java
@@ -0,0 +1,17 @@
+package com.semenindonesia.plantapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Lenovo on 7/4/2017.
+ */
+
+public class Main extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
