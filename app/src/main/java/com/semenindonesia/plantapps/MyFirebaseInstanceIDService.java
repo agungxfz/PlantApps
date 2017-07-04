1f5fbf93e8cdecf369a87b577718a4dc2bedb470
agungxfz <agungxfz@gmail.com>
2017-07-04 13:25:06 +0700

    Add FCM Service

--- app/src/main/java/com/semenindonesia/plantapps/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/semenindonesia/plantapps/MyFirebaseInstanceIDService.java
@@ -1,10 +1,30 @@
 package com.semenindonesia.plantapps;
 
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 /**
  * Created by Lenovo on 7/4/2017.
  */
 
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService{
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
 }
