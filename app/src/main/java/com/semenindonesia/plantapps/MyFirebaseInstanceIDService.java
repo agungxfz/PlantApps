efda35f4987909ca4cc641d2e282acaa8be1346d
agungxfz <agungxfz@gmail.com>
2017-07-04 13:20:56 +0700

    first

---
+++ app/src/main/java/com/semenindonesia/plantapps/MyFirebaseInstanceIDService.java
@@ -0,0 +1,10 @@
+package com.semenindonesia.plantapps;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Lenovo on 7/4/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService{
+}
---
+++ app/src/main/java/com/semenindonesia/plantapps/MyFirebaseMessagingService.java
@@ -0,0 +1,9 @@
+package com.semenindonesia.plantapps;
+
+/**
+ * Created by Lenovo on 7/4/2017.
+ */
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
